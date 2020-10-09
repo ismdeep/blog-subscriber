162a2b5f3c2a9548e37afb7d10aeebf51801f7cb
ismdeep <ismdeep@protonmail.com>
2020-10-09 15:38:51 +0800

    remove: tony_bai_test.py

--- tests/tony_bai_test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from sites.tony_bai_crawler import TonyBaiCrawler
-
-
-class TonyBaiCrawlerTester(unittest.TestCase):
-    def test_fetch(self):
-        posts = TonyBaiCrawler.fetch()
-        for post in posts:
-            print(post)
